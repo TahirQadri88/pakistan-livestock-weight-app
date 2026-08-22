0749ad81011bb19a619237402f104d241dcc9012
TahirQadri88 <mtahir001@hotmail.com>
2026-08-22 19:24:20 +0500

    Refresh offline cache for redesign v2

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE = 'plw-v4';
-const ASSETS = ['./','./index.html','./css/app.css','./js/app.js','./js/estimator.js','./js/storage.js','./manifest.webmanifest','./assets/icon.svg'];
-self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS)).then(()=>self.skipWaiting())));
-self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key.startsWith('plw-')&&key!==CACHE).map(key=>caches.delete(key)))).then(()=>self.clients.claim())));
-self.addEventListener('fetch',event=>{if(event.request.method!=='GET')return;event.respondWith(caches.match(event.request).then(cached=>cached||fetch(event.request).then(response=>{if(response.ok&&new URL(event.request.url).origin===location.origin)caches.open(CACHE).then(cache=>cache.put(event.request,response.clone()));return response;}).catch(()=>caches.match('./index.html'))));});
+const CACHE='plw-v5';
+const ASSETS=['./','./index.html','./css/app.css','./js/app.js','./js/estimator.js','./manifest.webmanifest','./assets/icon.svg'];
+self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
+self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k.startsWith('plw-')&&k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
+self.addEventListener('fetch',event=>{if(event.request.method!=='GET')return;event.respondWith(caches.match(event.request).then(cached=>cached||fetch(event.request).then(response=>{if(response.ok&&new URL(event.request.url).origin===location.origin)caches.open(CACHE).then(c=>c.put(event.request,response.clone()));return response;}).catch(()=>caches.match('./index.html'))));});

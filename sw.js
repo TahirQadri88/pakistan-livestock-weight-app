de06a2443957389e4a19a05fcf2b2314b1904a0a
TahirQadri88 <mtahir001@hotmail.com>
2026-08-22 18:28:12 +0500

    Bump service worker cache after localization fixes

--- sw.js
+++ sw.js
@@ -1,6 +1,5 @@
-const CACHE = 'plw-v2';
+const CACHE = 'plw-v3';
 const ASSETS = ['./', './index.html', './css/app.css', './js/app.js', './js/estimator.js', './js/storage.js', './js/i18n.js', './manifest.webmanifest', './assets/icon.svg'];
 self.addEventListener('install', event => event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS)).then(() => self.skipWaiting())));
 self.addEventListener('activate', event => event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(key => key.startsWith('plw-') && key !== CACHE).map(key => caches.delete(key)))).then(() => self.clients.claim())));
 self.addEventListener('fetch', event => { if (event.request.method !== 'GET') return; event.respondWith(caches.match(event.request).then(cached => cached || fetch(event.request).then(response => { if (response.ok && new URL(event.request.url).origin === location.origin) caches.open(CACHE).then(cache => cache.put(event.request, response.clone())); return response; }).catch(() => caches.match('./index.html')))); });
-
